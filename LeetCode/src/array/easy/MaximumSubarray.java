612026f3e65b9986297fad78094e44145eb19b7a
amitg <amitg@LAPTOP-CFJVV32J>
2020-04-04 08:37:13 +0530

    Solving Max Subarray Problem Using Brute Force Approach

--- LeetCode/src/array/easy/MaximumSubarray.java
+++ LeetCode/src/array/easy/MaximumSubarray.java
@@ -13,11 +13,46 @@ Explanation: [4,-1,2,1] has the largest sum = 6.
 
 package array.easy;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+/**
+ * @author amitg
+ *
+ */
 public class MaximumSubarray {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			System.out.println(" Enter The Size of Array");
+			int arraySize = Integer.parseInt(br.readLine());
+			int[] intArray = new int[arraySize];
+			System.out.println(" Enter Elements of Given Array ");
+			String[] stringIntegerArray = br.readLine().split("\\s");
+			for (int arrayIndex = 0; arrayIndex < arraySize; arrayIndex++) {
+				intArray[arrayIndex] = Integer.parseInt(stringIntegerArray[arrayIndex]);
+			}
+			// Using Brute Force Approach  To Solve 
+			int largetSubarraySum = maxSubArray(intArray);
+			System.out.println(" Larget Sum of contiguous Subarray is = " + largetSubarraySum);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
 	}
 
+	/**
+	 * @param nums
+	 * @return
+	 */
+	public static int maxSubArray(int[] nums) {
+		int largetSubarraySum;
+		return largetSubarraySum;
+	}
 }
