6a1131dcd19a764ca1f1d7cb76ba787e4e8973a8
amitg <amitg@LAPTOP-CFJVV32J>
2020-04-04 11:17:15 +0530

    Brute Force Approach is working but taking Too Long Time

--- LeetCode/src/array/easy/MaximumSubarray.java
+++ LeetCode/src/array/easy/MaximumSubarray.java
@@ -15,7 +15,9 @@ package array.easy;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.Reader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * @author amitg
@@ -30,17 +32,22 @@ public class MaximumSubarray {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		try {
-			System.out.println(" Enter The Size of Array");
-			int arraySize = Integer.parseInt(br.readLine());
-			int[] intArray = new int[arraySize];
-			System.out.println(" Enter Elements of Given Array ");
-			String[] stringIntegerArray = br.readLine().split("\\s");
-			for (int arrayIndex = 0; arrayIndex < arraySize; arrayIndex++) {
-				intArray[arrayIndex] = Integer.parseInt(stringIntegerArray[arrayIndex]);
-			}
-			// Using Brute Force Approach  To Solve 
-			int largetSubarraySum = maxSubArray(intArray);
-			System.out.println(" Larget Sum of contiguous Subarray is = " + largetSubarraySum);
+			char userChoice;
+			do {
+				System.out.println(" Enter The Size of Array");
+				int arraySize = Integer.parseInt(br.readLine());
+				int[] intArray = new int[arraySize];
+				System.out.println(" Enter Elements of Given Array ");
+				String[] stringIntegerArray = br.readLine().split("\\s");
+				for (int arrayIndex = 0; arrayIndex < arraySize; arrayIndex++) {
+					intArray[arrayIndex] = Integer.parseInt(stringIntegerArray[arrayIndex]);
+				}
+				// Using Brute Force Approach To Solve
+				int largetSubarraySum = maxSubArray(intArray);
+				System.out.println(" Larget Sum of contiguous Subarray is = " + largetSubarraySum);
+				System.out.println(" Do you want to continue then Press Y ?");
+				userChoice = br.readLine().charAt(0);
+			} while (userChoice == 'Y' || userChoice == 'y');
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
@@ -52,7 +59,24 @@ public class MaximumSubarray {
 	 * @return
 	 */
 	public static int maxSubArray(int[] nums) {
-		int largetSubarraySum;
-		return largetSubarraySum;
+		int largestSubarraySum;
+		int tempSum;
+		Set<Integer> tempSumStore = new HashSet<Integer>();
+		for (int firstPointerIndex = 0; firstPointerIndex < nums.length; firstPointerIndex++) {
+			tempSum = nums[firstPointerIndex];
+			tempSumStore.add(tempSum);
+			for (int secondPointerIndex = firstPointerIndex
+					+ 1; secondPointerIndex < nums.length; secondPointerIndex++) {
+				tempSum = tempSum + nums[secondPointerIndex];
+				tempSumStore.add(tempSum);
+			}
+
+		}
+		TreeSet<Integer> sortedSumStore = new TreeSet<Integer>(tempSumStore);
+		if (nums.length == 0) {
+			return 0;
+		} else
+			largestSubarraySum = sortedSumStore.last();
+		return largestSubarraySum;
 	}
 }
